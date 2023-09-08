a2af6c40622f1efecaa16c6ea9039a3c0473afc7
kc12341 <kylechen@andrew.cmu.edu>
2023-09-07 21:33:50 -0400

    added interfaces and adjusted getPostSummaryByPids types

--- src/posts/summary.ts
+++ src/posts/summary.ts
@@ -6,19 +6,48 @@ import * as plugins from '../plugins';
 import * as categories from '../categories';
 import * as utils from '../utils';
 
+// interface post_specs {
+    
+// }
+
 interface options_type {
     hasOwnProperty: (property: string) => boolean;
     stripTags: string | boolean;
     parse: string | boolean;
-    extraFields: [];
+    extraFields: string[];
 }
-interface posts_specs {
+
+interface topic_field {
+    cid: number;
+    mainPid: number;
 }
-interface posts_param {
+interface post_specs {
+    uid: number;
+    tid: number;
+    cid: number;
+    pid: number;
+    user: object;
+    handle: undefined;
+    topic: topic_field;
+    category: boolean;
+    isMainPost: boolean;
+    deleted: boolean;
+    timestamp: Date;
+    timestampISO: string;
+}
+interface post_fields {
+    filter: (filter_fn: object) => post_fields;
+    map: (p: any) => boolean[];
+    forEach: (post: (post: post_specs) => void) => void;
+}
+
+interface post_param {
     getPostSummaryByPids: (pids: number, uid: number, options: options_type) => Promise<any>;
-    getPostsFields: (pids: number, fields: string[]) => posts_specs;
+    getPostsFields: (pids: number, fields: string[]) => post_fields;
+    overrideGuestHandle: (post: post_specs, handle: undefined) => void; //unsure
+    parsePost: (post: post_specs) => post_fields;
 }
-export default function (Posts: posts_param) {
+export default function (Posts: post_param) {
     Posts.getPostSummaryByPids = async function (pids: number, uid: number, options: options_type) {
         if (!Array.isArray(pids) || !pids.length) {
             return [];
@@ -26,12 +55,12 @@ export default function (Posts: posts_param) {
         options.stripTags = options.hasOwnProperty('stripTags') ? options.stripTags : false;
         options.parse = options.hasOwnProperty('parse') ? options.parse : true;
         options.extraFields = options.hasOwnProperty('extraFields') ? options.extraFields : [];
-        const fields = ['pid', 'tid', 'content', 'uid', 'timestamp', 'deleted', 'upvotes', 'downvotes', 'replies', 'handle'].concat(options.extraFields);
-        let posts = await Posts.getPostsFields(pids, fields);
+        const fields: string[] = ['pid', 'tid', 'content', 'uid', 'timestamp', 'deleted', 'upvotes', 'downvotes', 'replies', 'handle'].concat(options.extraFields);
+        let posts: post_fields = Posts.getPostsFields(pids, fields);
         posts = posts.filter(Boolean);
         posts = await user.blocks.filter(uid, posts);
-        const uids = _.uniq(posts.map(p => p && p.uid));
-        const tids = _.uniq(posts.map(p => p && p.tid));
+        const uids: boolean[] = _.uniq(posts.map(p => p && p.uid));
+        const tids: boolean[] = _.uniq(posts.map(p => p && p.tid));
         const [users, topicsAndCategories] = await Promise.all([
             user.getUsersFields(uids, ['uid', 'username', 'userslug', 'picture', 'status']),
             getTopicAndCategories(tids),
@@ -51,7 +80,7 @@ export default function (Posts: posts_param) {
             post.topic = tidToTopic[post.tid];
             post.category = post.topic && cidToCategory[post.topic.cid];
             post.isMainPost = post.topic && post.pid === post.topic.mainPid;
-            post.deleted = post.deleted === 1;
+            post.deleted = false;
             post.timestampISO = utils.toISOString(post.timestamp);
         });
         posts = posts.filter(post => tidToTopic[post.tid]);
